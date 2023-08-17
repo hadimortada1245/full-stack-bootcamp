46ace941dcaaf9166573b7a5946395b3bf77ee46
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-08-17 11:12:20 +0300

    Challenge 2 - Done with step 1

---
+++ JavaScript Challenges/Challenge 2/script.js
@@ -0,0 +1,13 @@
+let age =22;
+if(age >=20)
+    console.log("You can enter this room.");
+else
+    console.log("You can't enter this room.");
+    const expression1 = 5 > 3;// true
+    const expression2 = 10 === '10';// false
+    const expression3 = true && false;// false
+    const expression4 = !expression3;//true
+    console.log(expression1);
+    console.log(expression2);
+    console.log(expression3);
+    console.log(expression4);
