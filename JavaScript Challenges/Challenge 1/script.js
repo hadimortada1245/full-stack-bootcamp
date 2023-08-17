3e83b68cc7c7b7c74c23c283971cd0efb7a60ad0
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-08-17 10:10:04 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,2 +1,11 @@
 console.log('I am linked now!');
 const name="Hadi Mortada";
+alert(name);
+let age =22;//It's OK change the values of variables when we use var and let 
+var lastName="Mortada";
+const DOB="2002-08-04";
+/*DOB="2002-08-03";script.js:7 Uncaught TypeError: Assignment to constant variable.
+at script.js:7:4*/
+console.log(age);
+console.log(lastName);
+console.log(DOB);
