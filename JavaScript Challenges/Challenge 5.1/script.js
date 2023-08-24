a58e58f5222b7ddfce5f63b26871ca29fc90fe45
Hadi Mortada <hadi.mortada@isae.edu.lb>
2023-08-24 15:36:54 +0300

    Challenge 5.1 - Done with step 2

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -14,3 +14,22 @@ newbtn.style.color="white";
 newbtn.innerHTML="clicked "+parseInt( Math.random()*1000+1);
 
 });
+let newH1=document.createElement("h1");
+newH1.innerHTML="Hadi Mortada";
+body.appendChild(newH1);
+newH1.style.width="25";
+newH1.style.color="black";
+newH1.style.borderBottom="1px dotted #000";
+document.addEventListener("keydown",function(event){
+    if(event.key === "a" || event.key === "A"){
+        newH1.style.backgroundColor="red";
+    }else if(event.key === "s" || event.key === "S")
+            newH1.style.marginLeft="10px";
+     else if(event.key === "d" || event.key === "D"){
+        let newP=document.createElement("p");
+        newP.innerHTML="Key D was pressed!";
+        body.appendChild(newP);
+     }else if(event.key === "w" || event.key === "W")
+                newH1.style.display="none";
+      else if(event.key === " ")newH1.style.fontSize="27px";                 
+});
